32ebf00d18bbda41150b6cb2955264a4ae62ed2e
Veronica Yexaling Jaquez Rojas <veronica.jaquez@mds-sistemas.com>
2017-11-13 09:47:12 -0700

    Prueba de Git

--- Indelek_2015/Indelek_2015/Formularios/Cerrar_Sesion.aspx.vb
+++ Indelek_2015/Indelek_2015/Formularios/Cerrar_Sesion.aspx.vb
@@ -1,6 +1,8 @@
 ﻿Public Class Cerrar_Sesion
     Inherits System.Web.UI.Page
 
+    Dim lPrueba As Boolean = False
+
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Session.Clear()
         Session.Abandon()
